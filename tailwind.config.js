5bf9be3ca2468960763c47df43c7f36252c5838e
Fercho5656 <morenomoranfernando@gmail.com>
2022-11-13 23:09:26 -0600

    Add height and width screen props

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,18 @@ module.exports = {
       transitionProperty: {
         'height': 'height',
         'width': 'width',
+      },
+      height: {
+        'screen/2': '50vh',
+        'screen/3': '33.333333vh',
+        'screen/4': '25vh',
+        'screen/5': '20vh',
+      },
+      width: {
+        'screen/2': '50vw',
+        'screen/3': '33.333333vw',
+        'screen/4': '25vw',
+        'screen/5': '20vw',
       }
     },
   },
