388d07b9ddd5eda7abdd2ddf994b9be9e6516905
Fercho5656 <morenomoranfernando@gmail.com>
2022-11-13 23:10:30 -0600

    Add table reusable components

---
+++ components/F/Table.vue
@@ -0,0 +1,12 @@
+<template>
+  <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+    <slot />
+  </table>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+
+</style>
---
+++ components/F/Thead.vue
@@ -0,0 +1,12 @@
+<template>
+  <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+    <slot />
+  </thead>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+
+</style>
