ec139e87840baaac28967a94e106beb76e2d3be0
Fercho5656 <morenomoranfernando@gmail.com>
2022-11-20 16:44:40 -0600

    Add related property

--- interfaces/IClient.ts
+++ interfaces/IClient.ts
@@ -3,9 +3,10 @@ import IZone from "./IZone"
 export default interface IClient {
   id?: number
   name: string
+  address: string
   lat: string
   lng: string
   zone_id: number
-  zone: IZone
+  zone?: IZone
   created_at?: string
 }
