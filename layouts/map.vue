faf3891bc4964344d3d92d6c97efe67971ee96a4
Fercho5656 <morenomoranfernando@gmail.com>
2022-11-21 19:13:14 -0600

    Add map layout without padding

---
+++ layouts/map.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="w-screen h-screen bg-gray-200 dark:bg-slate-800 grid grid-cols-12">
+    <div class="col-span-12">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+</style>
