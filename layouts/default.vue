7d24224dcec5670e2c761970dec57a090e9d2725
Fercho5656 <morenomoranfernando@gmail.com>
2022-12-03 19:47:58 -0600

    Adjust styles

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="w-screen h-screen bg-gray-200 dark:bg-slate-800 grid grid-cols-12">
-    <div class="col-span-12 p-5">
+  <div class="bg-gray-200 dark:bg-slate-800 grid grid-cols-12">
+    <div class="h-screen col-span-12 p-5">
       <slot />
     </div>
   </div>
