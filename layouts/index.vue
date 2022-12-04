c9f0dc4c821072749a58f934c8d38fceb7db66dd
Fercho5656 <morenomoranfernando@gmail.com>
2022-12-03 19:51:18 -0600

    Add layout

---
+++ layouts/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="bg-gray-200 dark:bg-slate-800 grid grid-cols-12">
+    <div class="h-screen col-span-12 sm:col-span-8 sm:col-start-3 p-5">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+</style>
